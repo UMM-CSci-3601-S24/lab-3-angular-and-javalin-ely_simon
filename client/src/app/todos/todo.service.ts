77dcf1a0ce42deb30fe68aeb1d31112dd3816605
Simon <simonhm31@gmail.com>
2024-02-08 13:31:35 -0600

    Made getTodos method in todo.service.ts Co-authored-by: Ely Johnson <elyjohnson6@gmail.com>

--- client/src/app/todos/todo.service.ts
+++ client/src/app/todos/todo.service.ts
@@ -6,10 +6,30 @@ import { Todo } from './todo';
 
 @Injectable()
 export class TodoService {
-  readonly userUrl: string = environment.apiUrl + 'todos';
+  readonly todoUrl: string = environment.apiUrl + 'todos';
 
   constructor(private httpClient: HttpClient) {
   }
 
-  getTodos(filters?: { })
+  getTodos(filters?: { status?: boolean; bodyText?: string; owner?: string; category?: string}): Observable<Todo[]> {
+    let httpParams: HttpParams = new HttpParams();
+    if (filters) {
+      if (filters.status) {
+        httpParams = httpParams.set('status', filters.status.toString());
+      }
+      if (filters.owner) {
+        httpParams = httpParams.set('owner', filters.owner);
+      }
+      if (filters.bodyText) {
+        httpParams = httpParams.set('bodyText', filters.bodyText);
+      }
+      if (filters.category) {
+        httpParams = httpParams.set('category', filters.category);
+      }
+    }
+
+    return this.httpClient.get<Todo[]>(this.todoUrl, {
+      params: httpParams,
+    })
+  }
 }
