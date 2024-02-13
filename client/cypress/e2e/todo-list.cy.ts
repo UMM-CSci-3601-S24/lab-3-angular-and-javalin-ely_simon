0e47ec5b54173953e3b3875606f8fbfa77aed30c
Simon <simonhm31@gmail.com>
2024-02-12 18:17:49 -0600

    Added e2e testing for filtering by owner
    Co-authored-by: Ely Johnson <elyjohnson6@gmail.com>

--- client/cypress/e2e/todo-list.cy.ts
+++ client/cypress/e2e/todo-list.cy.ts
@@ -16,5 +16,18 @@ describe('Todo list', () => {
     page.getTodoTitle().should('have.text', 'Todos');
   });
 
-  
+  it('Should type something in the owner filter and check that it returned correct elements', () => {
+
+    cy.get('[data-test=todoOwnerInput]').type('Fry');
+
+    page.getTodoListItems().each($todo => {
+      cy.wrap($todo).find('.todo-list-owner').should('contain', 'Fry');
+    });
+
+    // (We check this two ways to show multiple ways to check this)
+    page.getTodoListItems().find('.todo-list-owner').each($owner =>
+      expect($owner.text()).to.equal('Fry')
+    );
+  });
+
 });
